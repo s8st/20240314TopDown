658cf18b88cb5bc994400d7c3dc5fd12339dc495
s8st <153998744+s8st@users.noreply.github.com>
2024-03-14 20:14:20 +0900

    feat_타일맵 추가

--- Assets/Scripts/Controllers/PlayerInputController.cs
+++ Assets/Scripts/Controllers/PlayerInputController.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,14 +14,14 @@ public class PlayerInputController : TopDownCharacterController
 
     public void OnMove(InputValue value)
     {
-        // Debug.Log("OnMove" + value.ToString());
+         Debug.Log("OnMove" + value.ToString());
         Vector2 moveInput = value.Get<Vector2>().normalized;
         CallMoveEvent(moveInput);
     }
 
     public void OnLook(InputValue value)
     {
-        // Debug.Log("OnLook" + value.ToString());
+         Debug.Log("OnLook" + value.ToString());
         Vector2 newAim = value.Get<Vector2>();
         Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
         newAim = (worldPos - (Vector2)transform.position).normalized;
@@ -35,6 +35,6 @@ public class PlayerInputController : TopDownCharacterController
 
     public void OnFire(InputValue value)
     {
-        Debug.Log("OnFire" + value.ToString());
+       Debug.Log("OnFire" + value.ToString());
     }
 }
